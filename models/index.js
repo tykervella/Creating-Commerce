5300d72ad02ec526f764180a6b2b2addbb35e9f6
tykervella <kervellatyler@gmail.com>
2023-04-01 20:12:42 -0700

    added associations, wrote get route for /api/categories

--- models/index.js
+++ models/index.js
@@ -4,13 +4,35 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// A category can have many products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
-// Categories have many Products
+// A product belongs to a single category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
+
+ProductTag.belongsTo(Product, {
+  foreignKey: "product_id"
+});
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: "tag_id"
+});
+
 
 module.exports = {
   Product,
